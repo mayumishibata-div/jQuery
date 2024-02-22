34ffc5d32b72c7834d548478fc4702b5e2b70fb3
mayumishibata-div <mayumi.shibata@di-v.co.jp>
2024-02-22 20:41:55 +0900

    slickを使ったスライド

---
+++ Clothes/js/script.js
@@ -0,0 +1,12 @@
+$(function(){
+  $('.slider-list').slick({
+      accessibility: false,
+      fade: true,
+      autoplay: true,
+      infinite: true,
+      dots: false,
+      arrows: false,
+      autoplaySpeed: 1000,
+      speed: 1000,
+  });
+});
