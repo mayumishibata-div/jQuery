bc647b7baa273abc8bfbdac8c0addd18afdfbab1
mayumishibata-div <mayumi.shibata@di-v.co.jp>
2024-02-27 23:06:28 +0900

    アコーディオンメニュー(矢印の実装）

--- GREENTREE/js/script.js
+++ GREENTREE/js/script.js
@@ -1,14 +1,14 @@
 $(function(){
 
-  /* ここにjQueryのコードを書く */
-  $("h1").hover(function(){
-    $("h1").css("color","red")
-  })
+  // /* ここにjQueryのコードを書く */
+  // $("h1").hover(function(){
+  //   $("h1").css("color","red")
+  // })
 
-  $(".question-contact").on("click",function(){
-    let accordion = $(this).next(".question-contact-answer");
-    $(accordion).slideToggle();
-  })
-  
-  
+    $(".question-contact").on("click",function(){
+      let accordion = $(this).next(".question-contact-answer");
+      $(accordion).slideToggle(300);
+      $(this).toggleClass("open", 300)
+    })
+
   });
