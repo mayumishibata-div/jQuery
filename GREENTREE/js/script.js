6fcbdc2ad908913530229750a2e515d85bbab1b3
mayumishibata-div <mayumi.shibata@di-v.co.jp>
2024-03-02 20:46:37 +0900

    グローバルナビゲーション

--- GREENTREE/js/script.js
+++ GREENTREE/js/script.js
@@ -5,14 +5,14 @@ $(function(){
   //   $("h1").css("color","red")
   // })
 
+  // アコーディオンメニュー
     $(".question-contact").on("click",function(){
       let accordion = $(this).next(".question-contact-answer");
       $(accordion).slideToggle(300);
       $(this).toggleClass("open", 300)
     })
 
-  });
-
+  // 画像を大きくしながらスライド
   const swiper = new Swiper(".swiper", {
     loop: true,
     effect: "fade", // フェード切り替え
@@ -28,16 +28,34 @@ $(function(){
     },
   });
 
+  // スクロール
+  // ヘッダーの高さを取り除く
   $(function () {
     const height = $(".header").height();
     $("main").css("margin-top", height);
   });
   
-
   $(".header-nav-list a[href*='#']").click(function(){
     const pageLink = $(this).attr("href");
     const pos = $(pageLink).offset().top;
     $("body, html").animate({scrollTop: pos},500);
     return false;
   });
+
+  // ハンバーガー
+  $(".hamburger").on("click",function(){
+    $(this).toggleClass("active");
+  // グローバルナビゲーション
+    $("#g-nav").toggleClass("open-active")
+  })
+
+  $("#g-nav a").on("click",function(){
+    $(".hamburger").removeClass("active");
+  // グローバルナビゲーション
+    $("#g-nav").removeClass("open-active")
+  })
+
+  
+});
+
   
